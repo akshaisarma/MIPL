5e6ae07eeb098051e4cd3340319ee42d628c2b2f
Jinhyung Park <jp2105@columbia.edu>
2012-03-24 20:24:27 -0400

    [matops] Issue #14 : Finished

--- src/edu/columbia/mipl/matops/DefaultMatrixOperations.java
+++ src/edu/columbia/mipl/matops/DefaultMatrixOperations.java
@@ -201,7 +201,16 @@ public class DefaultMatrixOperations implements MatrixOperations {
 	}
 
 	public PrimitiveArray div(final PrimitiveArray arg1, final PrimitiveArray arg2) {
-		return null;
+		if (!arg1.equalsDimensionally(arg2))
+			/* throw new UncompatiableMatrixDimensionException() */;
+		if (arg1.getRow() != arg1.getCol())
+			/* throw new UncompatiableMatrixDimensionException() */;
+
+		PrimitiveDoubleArray invArg2 = (PrimitiveDoubleArray) this.inverse(arg2);
+		PrimitiveDoubleArray result = 
+			(PrimitiveDoubleArray) this.mult(arg1, (PrimitiveArray) invArg2);
+
+		return result;
 	}
 
 	public PrimitiveArray div(final PrimitiveArray arg1, final double arg2) {
@@ -310,33 +319,153 @@ public class DefaultMatrixOperations implements MatrixOperations {
 		}
 		return matT;
 	}
+	
+	/* Private Functions for calculating inverse matrix */
+	public double determinant(final PrimitiveArray arg1) {
+		PrimitiveDoubleArray a1 = (PrimitiveDoubleArray) arg1;
+		int row = arg1.getRow();
+		int col = arg1.getCol();
+		double[][] temp = new double[row][row];
 
+		for (int i = 0; i < row; i++)
+			for (int j = 0; j < row; j++)
+				temp[i][j] = (Double) a1.getValue(i, j);
+
+		for (int n = 0; n < temp.length; n++) {
+			if (temp[n][n] == 0) {
+				for (int a = n; a < temp.length; a++) {
+					if (temp[a][n] != 0) {
+						for (int b = n; b < temp.length; b++) {
+							temp[n][b] += temp[a][b];
+						}
+						break;
+					}
+				}
+				if (temp[n][n] == 0) {
+					System.out.println(n + "ss");
+					return 0;
+				}
+			}
+			for (int i = n + 1; i < temp.length; i++) {
+				for (int j = temp[0].length - 1; j >= n; j--) {
+					temp[i][j] = temp[i][j] - temp[i][n] * temp[n][j] / temp[n][n];
+				}
+			}
+		}
+		double sum = 1;
+		for (int i = 0; i < temp.length; i++) {
+			sum *= temp[i][i];
+		}
+		return sum;
+	}
+
+	public PrimitiveArray minor(final PrimitiveArray arg1, int removeRow, int removeCol) {
+		int row = arg1.getRow();
+		int col = arg1.getCol();
+		int ar = 0, ac = 0, pos, offset = 0;
+		PrimitiveDoubleArray a1 = (PrimitiveDoubleArray) arg1;
+		double data[] = a1.getData();
+		double dataMinor[] = new double[(row - 1) * (col - 1)];
+		PrimitiveDoubleArray coMat = new PrimitiveDoubleArray(row - 1, col - 1, dataMinor);
+		for (int r = 0; r < row; r++) {
+			pos = offset;
+			for (int c = 0; c < col; c++) {
+				if (!(r == removeRow || c == removeCol)) {
+					coMat.setValue(ar, ac, (Double) data[pos]);
+					if (++ac >= col - 1) {
+						if (++ar < row - 1)
+							ac = 0;
+					}
+				}
+				pos++;
+			}
+			offset += arg1.getPaddedRow();
+		}
+		return coMat;
+	}
+
+	public double cofactor(PrimitiveArray arg1, int row, int col) {
+		PrimitiveArray matI = this.minor(arg1, row, col);
+		return this.determinant(matI) * Math.pow((-1), (row + col));
+	}
+
+	public PrimitiveArray unitMatrix(int k) {
+		PrimitiveDoubleArray r = new PrimitiveDoubleArray(k, k);
+		double[] data = r.getData();
+
+		int i, j, pos, offset = 0;
+
+		for (i = 0; i < r.getRow(); i++) {
+			pos = offset;
+			for (j = 0; j < r.getCol(); j++) {
+				data[pos] = 1;
+				pos++;
+			}
+			offset += r.getPaddedRow();
+		}
+
+		return r;
+	}
+
+	/* We do not use determinant to calculate inverse.
+	 * this inverse function uses the Gaussian elimination. */
 	public PrimitiveArray inverse(final PrimitiveArray arg1) {
 		int row = arg1.getRow();
 		int col = arg1.getCol();
-		int n = row;
-		if (row != col)
-			/* throw new UncompatiableMatrixDimensionException() */;
+		double[][] temp = new double[row][row];
 		PrimitiveDoubleArray a1 = (PrimitiveDoubleArray) arg1;
-		double data[] = a1.getData();
-		double dataI[] = new double[data.length];
-		PrimitiveDoubleArray matI = new PrimitiveDoubleArray(row, col, dataI);
 
-		this.assign((PrimitiveArray) matI, arg1);
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < row; j++) {
+				temp[i][j] = (Double) a1.getValue(i, j);
+			}
+		}
 
-		for (int k = 0; k < n - 1; ++k) {
-			for (int j = k + 1; j < n; ++j) {
-				double tmp = (Double) matI.getValue(k, j) / (Double) matI.getValue(k, k);
-				matI.setValue(k, j, (Double) tmp);
-				for (int i = k + 1; i < n; ++i) {
-					double tmp2 = (Double) matI.getValue(i, j) - 
-					(Double) matI.getValue(i, k) * (Double) matI.getValue(k, j);
-					matI.setValue(i, j, (Double) tmp2);
+		//PrimitiveDoubleArray a2 = (PrimitiveDoubleArray) this.unitMatrix(col);
+		double[][] comp = new double[col][col];
+		for (int i = 0; i < col; i++) {
+			for (int j = 0; j < col; j++) {
+				if (i == j)
+					comp[i][j] = 1;
+				else
+					comp[i][j] = 0;
+			}
+		}
+
+		for (int n = 0; n < col; n++) {
+			if (temp[n][n] == 0) {
+				for (int k = n; k < col; k++) {
+					if (temp[k][n] != 0) {
+						for (int a = 0; a < col; a++) {
+							comp[n][a] = comp[n][a] + comp[k][a];
+							temp[n][a] = temp[n][a] + temp[k][a];
+						}
+						break;
+					}
+				}
+			}
+			double p = temp[n][n];
+			for (int a = 0; a < col; a++) {
+				comp[n][a] /= p;
+				temp[n][a] /= p;
+			}
+			for (int i = 0; i < col; i++) {
+				if (i != n) {
+					double p1 = temp[i][n];
+					for (int j = col - 1; j >= 0; j--) {
+						comp[i][j] -= p1 * comp[n][j];
+						temp[i][j] -= p1 * temp[n][j];
+					}
 				}
 			}
 		}
 
-		return matI;
+		PrimitiveDoubleArray r = new PrimitiveDoubleArray(arg1.getRow(), arg1.getCol());
+		for (int i = 0; i < arg1.getRow(); ++i)
+			for (int j = 0; j < arg1.getCol(); ++j)
+				r.setValue(i, j, (Double) comp[i][j]);
+
+		return r;
 	}
 
 	public PrimitiveArray mod(final PrimitiveArray arg1, PrimitiveArray arg2) {
@@ -365,15 +494,74 @@ public class DefaultMatrixOperations implements MatrixOperations {
 	}
 
 	public double sum(final PrimitiveArray arg1) {
-		return 0;
+		PrimitiveDoubleArray a1 = (PrimitiveDoubleArray) arg1;
+		double[] data1 = a1.getData();
+		double result = 0;
+
+		int i, j, pos, offset = 0;
+
+		for (i = 0; i < arg1.getRow(); i++) {
+			pos = offset;
+			for (j = 0; j < arg1.getCol(); j++) {
+				result += data1[pos];
+				pos++;
+			}
+			offset += arg1.getPaddedRow();
+		}
+
+		return result;
 	}
+
 	public double mean(final PrimitiveArray arg1) {
-		return 0;
+		double sum = this.sum(arg1);
+		return sum / (arg1.getRow() * arg1.getCol());
 	}
+
 	public PrimitiveArray rowsum(final PrimitiveArray arg1) {
-		return null;
+		PrimitiveDoubleArray a1 = (PrimitiveDoubleArray) arg1;
+		PrimitiveDoubleArray result = new PrimitiveDoubleArray(1, arg1.getCol());
+		double[] data1 = a1.getData();
+
+		int i, j, pos, offset = 0;
+
+		for (i = 0; i < arg1.getRow(); i++) {
+			pos = offset;
+			for (j = 0; j < arg1.getCol(); j++) {
+				double val = (Double) result.getValue(0, j);
+				val = val + data1[pos];
+				result.setValue(0, j, (Double) val);
+				pos++;
+			}
+			offset += arg1.getPaddedRow();
+		}
+
+		return result;
 	}
+
 	public PrimitiveArray rowmean(final PrimitiveArray arg1) {
-		return null;
+		PrimitiveDoubleArray a1 = (PrimitiveDoubleArray) arg1;
+		PrimitiveDoubleArray result = new PrimitiveDoubleArray(1, arg1.getCol());
+		double[] data1 = a1.getData();
+
+		int i, j, pos, offset = 0;
+
+		for (i = 0; i < arg1.getRow(); i++) {
+			pos = offset;
+			for (j = 0; j < arg1.getCol(); j++) {
+				double val = (Double) result.getValue(0, j);
+				val = val + data1[pos];
+				result.setValue(0, j, (Double) val);
+				pos++;
+			}
+			offset += arg1.getPaddedRow();
+		}
+
+		for (j = 0; j < arg1.getCol(); j++) {
+			double val = (Double) result.getValue(0, j);
+			val = val / arg1.getRow();
+			result.setValue(0, j, (Double) val);
+		}
+
+		return result;
 	}
 }
--- test/unittest/src/edu/columbia/mipl/matops/MatrixOperationTest.java
+++ test/unittest/src/edu/columbia/mipl/matops/MatrixOperationTest.java
@@ -177,12 +177,102 @@ public class MatrixOperationTest extends TestCase {
 
 		assertTrue(mat.equalsSemantically(mat3x1_1));
 	}
-	public void testMatrix_Inverse() {
-		final PrimitiveDoubleArray mat3x3_3 = new PrimitiveDoubleArray(3, 3, data3x3_3);
-		PrimitiveArray matI = matOpObj.inverse((PrimitiveArray) mat3x3_3);
-		boolean result = true;
 
-		assertTrue(result == true);
+	public void testMatrix_Determinant() {
+		double data2x2_1[] = {2, 2, 1, 3};
+		final PrimitiveDoubleArray mat2x2_1 = new PrimitiveDoubleArray(2, 2, data2x2_1);
+
+		double det = matOpObj.determinant(mat2x2_1);
+
+		assertTrue(det == 4);
+	}
+
+	public void testMatrix_Minor() {
+		double copy_data3x3_1[] = {1, 2, 3, 1, 2, 3, 1, 2, 3};
+		double copy_data2x2_1[] = {1, 3, 1, 3};
+		final PrimitiveDoubleArray mat3x3_1 = new PrimitiveDoubleArray(3, 3, copy_data3x3_1);
+		final PrimitiveDoubleArray mat2x2_1 = new PrimitiveDoubleArray(2, 2, copy_data2x2_1);
+
+		/* remove 2nd row and 2nd col, so input row and col will be 1, 1 */
+		PrimitiveArray matI = matOpObj.minor((PrimitiveArray) mat3x3_1, 1, 1);
+
+		assertTrue(matI.equalsSemantically(mat2x2_1));
+	}
+
+	public void testMatrix_Inverse() {
+		double copy_data3x3_1[] = {1, -1, 1, 0, 2, -1, 2, 3, 0};
+		double i_data3x3_1[] = {1, 0, 0, 0, 1, 0, 0, 0, 1};
+		final PrimitiveDoubleArray mat3x3_1 = new PrimitiveDoubleArray(3, 3, copy_data3x3_1);
+		final PrimitiveDoubleArray i_mat3x3 = new PrimitiveDoubleArray(3, 3, i_data3x3_1);
+
+		PrimitiveArray matI = matOpObj.inverse((PrimitiveArray) mat3x3_1);
+		PrimitiveArray matII = matOpObj.mult(matI, mat3x3_1);
+
+		assertTrue(matII.equalsSemantically(i_mat3x3));
+	}
+	
+	public void testMatrix_div() {
+		double copy_data3x3_1[] = {1, -1, 1, 0, 2, -1, 2, 3, 0};
+		double i_data3x3_1[] = {1, 0, 0, 0, 1, 0, 0, 0, 1};
+		final PrimitiveDoubleArray mat3x3_1 = new PrimitiveDoubleArray(3, 3, copy_data3x3_1);
+		final PrimitiveDoubleArray mat3x3_2 = new PrimitiveDoubleArray(3, 3, copy_data3x3_1);
+		final PrimitiveDoubleArray i_mat3x3 = new PrimitiveDoubleArray(3, 3, i_data3x3_1);
+
+		/* mat3x3_1 and mat3x3_2 are same matrix,
+		 * so, the division result will be the identity matrix i_mat3x3 */
+		PrimitiveArray matI = matOpObj.div(mat3x3_1, mat3x3_2);
+
+		assertTrue(matI.equalsSemantically(i_mat3x3));
+	}
+
+	public void testMatrix_sum() {
+		double copy_data3x3_1[] = {1, -1, 1, 
+					   0, 2, -1, 
+					   2, 3, 0};
+		double result = 7;
+		final PrimitiveDoubleArray mat3x3_1 = new PrimitiveDoubleArray(3, 3, copy_data3x3_1);
+
+		double compare = matOpObj.sum(mat3x3_1);
+
+		assertTrue(compare == result);
+	}
+
+	public void testMatrix_rowsum() {
+		double copy_data3x3_1[] = {1, -1, 1, 
+					   0, 2, -1, 
+					   2, 3, 0};
+		double result_rowsum[] = {3, 4, 0};
+		final PrimitiveDoubleArray mat3x3_1 = new PrimitiveDoubleArray(3, 3, copy_data3x3_1);
+		final PrimitiveDoubleArray r_rowsum = new PrimitiveDoubleArray(1, 3, result_rowsum);
+
+		PrimitiveArray matI = matOpObj.rowsum(mat3x3_1);
+
+		assertTrue(r_rowsum.equalsSemantically(matI));
+	}
+
+	public void testMatrix_mean() {
+		double copy_data3x3_1[] = {1, -1, 1, 
+					   0, 2, -1, 
+					   2, 3, 0};
+		double result = 7.0/9;
+		final PrimitiveDoubleArray mat3x3_1 = new PrimitiveDoubleArray(3, 3, copy_data3x3_1);
+
+		double compare = matOpObj.mean(mat3x3_1);
+
+		assertTrue(compare == result);
+	}
+
+	public void testMatrix_rowmean() {
+		double copy_data3x3_1[] = {1, -1, 1, 
+					   0, 2, -1, 
+					   2, 3, 0};
+		double result_rowmean[] = {1, 4.0/3, 0};
+		final PrimitiveDoubleArray mat3x3_1 = new PrimitiveDoubleArray(3, 3, copy_data3x3_1);
+		final PrimitiveDoubleArray r_rowmean = new PrimitiveDoubleArray(1, 3, result_rowmean);
+
+		PrimitiveArray matI = matOpObj.rowmean(mat3x3_1);
+
+		assertTrue(r_rowmean.equalsSemantically(matI));
 	}
 
 	public static Test suite() {
