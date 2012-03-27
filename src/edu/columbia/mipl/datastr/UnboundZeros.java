eb60ad928711dfec140d616f2e1c121fc4f194dc
YoungHoon Jung <yj2244@columbia.edu>
2012-03-27 12:22:44 -0400

    add unbound builtin matrices - Issue #29

---
+++ src/edu/columbia/mipl/datastr/UnboundZeros.java
@@ -0,0 +1,22 @@
+/*
+ * MIPL: Mining Integrated Programming Language
+ *
+ * File: UnboundZeros.java
+ * Author: YoungHoon Jung <yj2244@columbia.edu>
+ * Reviewer: Younghoon Jeon <yj2231@columbia.edu>
+ * Description: Built-in Unbound Matrix Zeros
+ */
+package edu.columbia.mipl.datastr;
+
+import java.util.*;
+import java.lang.reflect.*;
+
+public class UnboundZeros extends UnboundMatrix {
+	public  String getName() {
+		return "zeros";
+	}
+
+	public Double getValue(int row, int col) {
+		return 0.0;
+	}
+}
