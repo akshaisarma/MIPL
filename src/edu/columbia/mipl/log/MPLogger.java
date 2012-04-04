7d2622b3f021efbd75b0af41204394105ed50dae
Jinhyung Park <jp2105@columbia.edu>
2012-04-04 17:58:53 -0400

    WIP : Logger

---
+++ src/edu/columbia/mipl/log/MPLogger.java
@@ -0,0 +1,50 @@
+/*
+ * MIPL: Mining Integrated Programming Language
+ *
+ * File: MPLogger.java
+ * Author: Jin Hyung Park (jp2105)
+ * Reviewer: YoungHoon Jung (yj2244)
+ * Description: MPLogger
+ */
+package edu.columbia.mipl.log;
+
+import org.apache.log4j.*;
+import java.util.*;
+
+public class MPLogger {
+	private static MPLogger instance = null;
+
+	MPLogger() {
+	}
+
+	public static synchronized MPLogger getInstance() {
+		if (instance == null) {
+			instance = new MPLogger();
+		}
+
+		return instance;
+	}
+
+	/*
+	public static void info(String log) {
+		Throwable t = new Throwable();
+		StackTraceElement methodCaller = t.getStackTrace()[1];
+		Logger logger = LogManager.getLogger(methodCaller.getClassName());
+		logger.info(log);
+	}
+
+	public static void warn(String log) {
+		Throwable t = new Throwable();
+		StackTraceElement methodCaller = t.getStackTrace()[1];
+		Logger logger = LogManager.getLogger(methodCaller.getClassName());
+		logger.warn(log);
+	}
+
+	public static void err(String log) {
+		Throwable t = new Throwable();
+		StackTraceElement methodCaller = t.getStackTrace()[1];
+		Logger logger = LogManager.getLogger(methodCaller.getClassName());
+		logger.warn(log);
+	}
+	*/
+}
