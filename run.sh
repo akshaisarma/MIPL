2110c5e42c7e60f372b79bef8e9a61abff3965d0
Jinhyung Park <jp2105@columbia.edu>
2012-04-11 17:25:48 -0400

    Fixed classification example

--- run.sh
+++ run.sh
@@ -6,7 +6,8 @@ echo "1. build the project"
 #ant
 
 echo "2. Compile the sample program into java source with MIPL compiler"
-java -cp build edu.columbia.mipl.Main test/input/hello.mipl
+#java -cp build edu.columbia.mipl.Main test/input/hello.mipl
+java -cp build edu.columbia.mipl.Main test/input/classification.mipl
 
 echo "3. compile the java source"
 javac -cp build build/MiplProgram.java
