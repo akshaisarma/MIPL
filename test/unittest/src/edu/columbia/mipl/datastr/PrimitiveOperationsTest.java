d59b845dc656f15d749f5020aa013a1cf38ec7aa
Akshai Sarma <akshaisarma@gmail.com>
2012-04-23 08:58:50 -0400

    [unittest]  Template of PrimitiveOperationsTest

---
+++ test/unittest/src/edu/columbia/mipl/datastr/PrimitiveOperationsTest.java
@@ -0,0 +1,96 @@
+/**
+ * MIPL: Mining Integrated Programming Language
+ *
+ * File: PrimitiveOperationsTest.java
+ * Author: Akshai Sarma <as4107@columbia.edu>
+ * Reviewer: Wonjoon Song <dws2127@columbia.edu>
+ * Description: Tests for Primitive Operations
+ */
+
+package edu.columbia.mipl.datastr;
+
+import java.util.*;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import edu.columbia.mipl.datastr.*;
+import edu.columbia.mipl.matops.*;
+
+public class PrimitiveOperationsTest extends TestCase {
+
+	protected PrimitiveOperations primOp;
+
+	public static void main(String args[]) {
+		junit.textui.TestRunner.run (suite());
+	}
+
+	@Override
+	protected void setUp() {
+	}
+
+	public static Test suite() {
+		return new TestSuite(PrimitiveOperationsTest.class);
+	}
+
+	public void testAssign() {
+
+	}
+
+	public void testOr() {
+
+	}
+
+	public void testAnd() {
+
+	}
+
+	public void testEqual() {
+
+	}
+
+	public void testNotEqual() {
+
+	}
+
+	public void testLessThan() {
+
+	}
+
+	public void testGreaterThan() {
+
+	}
+
+	public void testLessThanEqual() {
+
+	}
+
+	public void testGreaterThanEqual() {
+
+	}
+
+	public void testAddition() {
+
+	}
+
+	public void testSubtraction() {
+
+	}
+
+	public void testMultiplication() {
+
+	}
+
+	public void testDivisoin() {
+
+	}
+
+	public void testModulus() {
+
+	}
+
+	public void absoluteValue() {
+
+	}
+
+}
